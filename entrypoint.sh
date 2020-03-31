be2e2e720d84deda294987133b2c3dd05cdb44cd
mifonpe <mifonpe@gmail.com>
2020-03-31 19:31:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Requesting registration URL at '${registration_url}'"
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
-/home/github/config.sh \
+. ./config.sh \
     --name $(hostname) \
     --token ${RUNNER_TOKEN} \
     --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
@@ -14,10 +14,10 @@ export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
     --replace
 
 remove() {
-    /home/github/config.sh remove --unattended --token "${RUNNER_TOKEN}"
+    . ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
 }
 
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
 
-/home/github/run.sh "$*" &
+. ./run.sh "$*" &
